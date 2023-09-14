beff34580c0a48f13279fdc1a3c62ef7f62f661c
Ian <gichaneian@gmail.com>
2023-09-14 22:40:00 +0300

    Complete

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+    return a+b
+}
+add(1,80)
+function subtract(a,b){
+    return a-b
+}
+subtract(60,40)
+
+function multiply(a,b){
+    return a*b
+}
+multiply(2,3.4)
+
+function divide(a,b){
+    return a/b
+}
+divide(5.0,2.5)
+
+function increment(n){
+    return n+1
+}
+increment(49)
+
+
+function decrement(n){
+    return n-1
+}
+decrement(489)
+
+function makeInt(n){
+    return parseInt(n,10)
+}
+makeInt('489')
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+preserveDecimal(4.999999999999999999989)
